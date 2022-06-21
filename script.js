cb1e293ec4f713d21f9f33f2f7532424cb00a935
Engeniusam <engeniusam@gmail.com>
2022-06-21 22:42:55 +0300

    Updated timeout

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function typing() {
   if (i < text.length) {
     document.getElementById("info__subtitle").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing, 2000);
+    setTimeout(typing, 500);
   }
 }
 typing();
